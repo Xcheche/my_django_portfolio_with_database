40e524fcf734925e0b8507df6fa4b6a2d33a7f3d
xcheche <chekwubechukwu23@gmail.com>
2024-07-15 13:42:16 +0100

    added pagination

--- resume/views.py
+++ resume/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render,get_object_or_404
-from django .http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 from resume.models import About, HomeInfo, Projects, Experience, Certificates
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -21,22 +21,31 @@ def about(request):
 
 def projects(request):
     project_show = Projects.objects.all()
-    
-    return render(request, "projects.html", {"project_show": project_show})
+    paginator = Paginator(project_show, 1)
+    page = request.GET.get("page")
+    print(page)
+    try:
+        project_show = paginator.page(page)
+    except PageNotAnInteger:
+        project_show = paginator.page(1)
+    except EmptyPage:
+        project_show = paginator.page(paginator.num_pages)
+        print(project_show)
 
-
-
+    return render(
+        request, "projects.html", {"project_show": project_show, "page": page}
+    )
 
 
 def aboutproject(request, project_id):
     project = get_object_or_404(Projects, id=project_id)
-    print(project.title, project.decription) 
+    print(project.title, project.decription)
     return render(request, "aboutproject.html", {"project": project})
 
 
 def experience(request):
-  experience = Experience.objects.all()
-  return render(request, "experience.html", {"experience": experience})
+    experience = Experience.objects.all()
+    return render(request, "experience.html", {"experience": experience})
 
 
 def certificates(request):
@@ -47,13 +56,14 @@ def certificates(request):
 def contact(request):
     return render(request, "contact.html")
 
+
 def resume(request):
-    resume_path ="cv/python.pdf"
+    resume_path = "cv/python.pdf"
     resume_path = staticfiles_storage.path(resume_path)
-    if  staticfiles_storage.exists(resume_path):
-        with open(resume_path, 'rb') as resume_file:
-            response = HttpResponse(resume_file.read(), content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment; filename="python.pdf"'
+    if staticfiles_storage.exists(resume_path):
+        with open(resume_path, "rb") as resume_file:
+            response = HttpResponse(resume_file.read(), content_type="application/pdf")
+            response["Content-Disposition"] = 'attachment; filename="python.pdf"'
             return response
     else:
         return HttpResponse("Resume not found", status=404)
