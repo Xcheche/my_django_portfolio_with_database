e3680ac9e55dd9e1b0d3d10a90c31efc18c3efac
xcheche <chekwubechukwu23@gmail.com>
2024-09-22 01:57:51 +0100

    removed .env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("SECRET_KEY")
+SECRET_KEY = "django-insecure-c0+tv_e&x1f@8*q(&+!9@&ce882a7&h4no+zn33!sk&l@a9$9)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
